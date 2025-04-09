f92d86733ace7b60b45b03e2b3085677e412e23a
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-04-09 14:28:03 -0400

    Task Manager

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,5 +1,6 @@
 import cors from 'cors';
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { v4 as uuidv4 } from 'uuid';
 
 const host = process.env.HOST ?? '0.0.0.0';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
@@ -8,8 +9,123 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send({ message: 'Hello API' });
+enum Status {
+  OPEN = 'OPEN',
+  CLOSED = 'CLOSED',
+}
+
+interface Task {
+  id: any;
+  title: string;
+  description: string;
+  status: Status;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+let tasks: Task[] = [];
+
+// Endpoint to get all tasks
+app.get('/api/tasks', (req: Request, res: Response) => {
+  try {
+    res.status(200).json(tasks);
+  } catch (error) {
+    console.error('Error fetching tasks:', error);
+    res.status(500).send({ error: 'Error fetching tasks' });
+  }
+});
+
+// Endpoint to get a task by ID
+app.get('/api/tasks/:id', (req: Request, res: Response) => {
+  try {
+    const taskId = req.params.id;
+    const task = tasks.find((task) => task.id === taskId);
+    if (!task) {
+      return res.status(404).send({ error: 'task not found' });
+    }
+    return res.status(200).json(task);
+  } catch (error) {
+    console.error('Error fetching task:', error);
+    return res.status(500).send({ error: 'Error fetching task' });
+  }
+});
+
+// Endpoint to create a new task
+app.post('/api/tasks', (req: Request, res: Response) => {
+  try {
+    const { title, description } = req.body;
+    if (!title) {
+      return res.status(400).json({ message: 'Title is required' });
+    }
+    if (!description) {
+      return res.status(400).json({ message: 'Description is required' });
+    }
+
+    const newTask = {
+      id: uuidv4(),
+      title,
+      description,
+      status: Status.OPEN,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+    tasks.push(newTask);
+
+    return res.status(201).json(newTask);
+  } catch (error) {
+    console.error('Error creating task:', error);
+    return res.status(500).send({ error: 'Error creating task' });
+  }
+});
+
+// Endpoint to update an existing task
+app.patch('/api/tasks/:id', (req: Request, res: Response) => {
+  try {
+    const taskId = req.params.id;
+    const { title, description, status } = req.body;
+
+    const task = tasks.find((task) => task.id === taskId);
+    if (!task) {
+      return res.status(404).send({ error: 'task not found' });
+    }
+
+    const updatedTask = {
+      ...task,
+      title: title || task.title,
+      description: description || task.description,
+      status: status || task.status,
+      updatedAt: new Date(),
+    };
+    tasks = tasks.map((task) => (task.id === taskId ? updatedTask : task));
+
+    return res.status(200).json(updatedTask);
+  } catch (error) {
+    console.error('Error creating task:', error);
+    return res.status(500).send({ error: 'Error updating task' });
+  }
+});
+
+// Endpoint to delete a task
+app.delete('/api/tasks/:id', (req: Request, res: Response) => {
+  try {
+    const taskId = req.params.id;
+    const taskIndex = tasks.findIndex((task) => task.id === taskId);
+
+    if (taskIndex === -1) {
+      return res.status(404).json({ message: 'Task not found' });
+    }
+
+    // Remove task from the array
+    tasks.splice(taskIndex, 1);
+    return res.status(200).json({ message: 'Task deleted' });
+  } catch (error) {
+    console.error('Error creating task:', error);
+    return res.status(500).send({ error: 'Error deleting task' });
+  }
+});
+
+app.get('/', (req: Request, res: Response) => {
+  res.send({ message: 'Task Manager App Running' });
 });
 
 app.listen(port, host, () => {
