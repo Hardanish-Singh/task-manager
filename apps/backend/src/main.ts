69ad87926533df8fffbd7f57019c2c9e501894aa
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-04-09 11:32:34 -0400

    Task Manager

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,9 +1,12 @@
+import cors from 'cors';
 import express from 'express';
 
 const host = process.env.HOST ?? '0.0.0.0';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
 const app = express();
+app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send({ message: 'Hello API' });
