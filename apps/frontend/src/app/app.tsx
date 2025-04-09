201e651d68248eca910de5fcb94aa7ec6274ed4f
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-04-09 11:13:47 -0400

    Task Manager

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -1,21 +1,10 @@
-// Uncomment this line to use CSS modules
-// import styles from './app.module.css';
-import NxWelcome from './nx-welcome';
-
-import { Route, Routes, Link } from 'react-router-dom';
+// import { Link, Route, Routes } from 'react-router-dom';
 
 export function App() {
   return (
     <div>
-      <NxWelcome title="frontend" />
-
-      {/* START: routes */}
-      {/* These routes and navigation have been generated for you */}
-      {/* Feel free to move and update them to fit your needs */}
-      <br />
-      <hr />
-      <br />
-      <div role="navigation">
+      Hello World
+      {/* <div role="navigation">
         <ul>
           <li>
             <Link to="/">Home</Link>
@@ -43,8 +32,7 @@ export function App() {
             </div>
           }
         />
-      </Routes>
-      {/* END: routes */}
+      </Routes> */}
     </div>
   );
 }
