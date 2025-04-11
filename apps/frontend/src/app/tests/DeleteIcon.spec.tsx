7e44853b30d155f3faffbe33dcb1570b48d25176
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-04-10 21:04:32 -0400

    Task Manager

---
+++ apps/frontend/src/app/tests/DeleteIcon.spec.tsx
@@ -0,0 +1,45 @@
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import DeleteIcon from '../components/DeleteIcon';
+
+const mockUpdateTasks = jest.fn();
+
+jest.spyOn(console, 'warn').mockImplementation(() => {});
+jest.spyOn(console, 'error').mockImplementation(() => {});
+
+describe('DeleteIcon', () => {
+  const taskId = 'cd6c9362-96cb-4604-9506-c36dc6e2974f';
+  const taskTitle = 'Test Task';
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should show the ConfirmDeletion modal when the delete icon is clicked', () => {
+    render(
+      <DeleteIcon id={taskId} title={taskTitle} updateTasks={mockUpdateTasks} />
+    );
+    fireEvent.click(screen.getByTestId('delete-icon'));
+    expect(screen.getByText(`Delete ${taskTitle}`)).toBeTruthy();
+  });
+
+  it('should not call updateTasks when the cancel button is clicked in the modal', async () => {
+    render(
+      <DeleteIcon id={taskId} title={taskTitle} updateTasks={mockUpdateTasks} />
+    );
+    fireEvent.click(screen.getByTestId('delete-icon'));
+    fireEvent.click(screen.getByText('Cancel'));
+    await waitFor(() => {
+      expect(mockUpdateTasks).not.toHaveBeenCalled();
+    });
+  });
+
+  it('should close the modal when the cancel button is clicked', () => {
+    render(
+      <DeleteIcon id={taskId} title={taskTitle} updateTasks={mockUpdateTasks} />
+    );
+    fireEvent.click(screen.getByTestId('delete-icon'));
+    expect(screen.getByText(`Delete ${taskTitle}`)).toBeTruthy();
+    fireEvent.click(screen.getByText('Cancel'));
+    expect(screen.queryByText(`Delete ${taskTitle}`)).toBeFalsy();
+  });
+});
---
+++ apps/frontend/src/app/tests/Edit.spec.tsx
@@ -0,0 +1,33 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import { BrowserRouter, useNavigate } from 'react-router-dom';
+import EditIcon from '../components/EditIcon';
+
+jest.spyOn(console, 'warn').mockImplementation(() => {});
+jest.spyOn(console, 'error').mockImplementation(() => {});
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: jest.fn(),
+}));
+
+describe('EditIcon Component', () => {
+  const mockNavigate = jest.fn();
+  const mockId = 'task-id';
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should navigate to the correct path when the edit icon is clicked', () => {
+    jest.mocked(useNavigate).mockReturnValue(mockNavigate);
+    render(
+      <BrowserRouter>
+        <EditIcon id={mockId} />
+      </BrowserRouter>
+    );
+    const editIcon = screen.getByTestId('edit-icon');
+    expect(editIcon).toBeTruthy();
+    fireEvent.click(editIcon);
+    expect(mockNavigate).toHaveBeenCalledWith(`/tasks/${mockId}`);
+  });
+});
