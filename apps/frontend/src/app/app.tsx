90fc312b32b21351707e5bcc8bb16b2076ae39ce
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-04-15 18:07:21 -0400

    Task Manager

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -2,50 +2,48 @@ import { Link, Route, Routes } from 'react-router-dom';
 import Tasks from './components/Tasks';
 import ViewTasks from './components/ViewTasks';
 
-const App = () => {
-  return (
-    <div className="flex flex-col min-h-screen">
-      <header className="bg-blue-500 text-white p-4">
-        <div className="container mx-auto flex justify-between items-center">
-          <h1 className="text-2xl font-bold">
-            <Link to="/" className="cursor-pointer">
-              Task Manager
-            </Link>{' '}
-          </h1>
-          <nav>
-            <ul className="flex space-x-6">
-              <li>
-                <Link to="/" className="hover:underline">
-                  Home
-                </Link>{' '}
-              </li>
-              <li>
-                <Link to="/tasks" className="hover:underline">
-                  Create Task
-                </Link>{' '}
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </header>
+const App = () => (
+  <div className="flex flex-col min-h-screen">
+    <header className="bg-blue-500 text-white p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <h1 className="text-2xl font-bold">
+          <Link to="/" className="cursor-pointer">
+            Task Manager
+          </Link>{' '}
+        </h1>
+        <nav>
+          <ul className="flex space-x-6">
+            <li>
+              <Link to="/" className="hover:underline">
+                Home
+              </Link>{' '}
+            </li>
+            <li>
+              <Link to="/tasks" className="hover:underline">
+                Create Task
+              </Link>{' '}
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
 
-      <main className="flex-grow p-6">
-        <div className="container mx-auto">
-          <Routes>
-            <Route path="/" element={<ViewTasks />} />
-            <Route path="/tasks" element={<Tasks />} />
-            <Route path="/tasks/:id" element={<Tasks />} />
-          </Routes>
-        </div>
-      </main>
+    <main className="flex-grow p-6">
+      <div className="container mx-auto">
+        <Routes>
+          <Route path="/" element={<ViewTasks />} />
+          <Route path="/tasks" element={<Tasks />} />
+          <Route path="/tasks/:id" element={<Tasks />} />
+        </Routes>
+      </div>
+    </main>
 
-      <footer className="bg-gray-800 text-white p-4 mt-auto">
-        <div className="container mx-auto text-center">
-          <p>&copy; 2025 Task Manager</p>
-        </div>
-      </footer>
-    </div>
-  );
-};
+    <footer className="bg-gray-800 text-white p-4 mt-auto">
+      <div className="container mx-auto text-center">
+        <p>&copy; 2025 Task Manager</p>
+      </div>
+    </footer>
+  </div>
+);
 
 export default App;
--- apps/frontend/src/app/tests/Tasks.spec.tsx
+++ apps/frontend/src/app/tests/Tasks.spec.tsx
@@ -34,22 +34,26 @@ describe('Tasks', () => {
     jest
       .spyOn(require('react-router-dom'), 'useParams')
       .mockReturnValue({ id: '123' });
+
     const { baseElement } = render(
       <BrowserRouter>
         <Tasks />
       </BrowserRouter>
     );
+
     expect(baseElement).toBeTruthy();
     expect(screen.getByText('Loading...')).toBeTruthy();
   });
 
   it('should populate the form', async () => {
     jest.spyOn(require('react-router-dom'), 'useParams').mockReturnValue({});
+
     render(
       <BrowserRouter>
         <Tasks />
       </BrowserRouter>
     );
+
     await waitFor(() => {
       expect(screen.getByText('Title')).toBeTruthy();
       expect(screen.getByText('Description')).toBeTruthy();
@@ -60,15 +64,18 @@ describe('Tasks', () => {
 
   it('should update input fields on change', async () => {
     jest.spyOn(require('react-router-dom'), 'useParams').mockReturnValue({});
+
     render(
       <BrowserRouter>
         <Tasks />
       </BrowserRouter>
     );
+
     await waitFor(() => {
       const titleInput = screen.getByPlaceholderText('Enter Title');
       fireEvent.change(titleInput, { target: { value: 'Updated Title' } });
       expect(screen.getByDisplayValue('Updated Title')).toBeTruthy();
+
       const descriptionInput = screen.getByPlaceholderText(
         'Write your description here...'
       );
@@ -77,25 +84,32 @@ describe('Tasks', () => {
       });
       expect(screen.getByDisplayValue('Updated Description')).toBeTruthy();
     });
+
     (axios.post as jest.Mock).mockResolvedValueOnce({
       status: 201,
       message: 'Task created successfully',
     });
+
     fireEvent.click(screen.getByText('Submit'));
+
     await waitFor(() => {
       expect(MockToast).toBeTruthy();
     });
   });
+
   it('should display an error message if fetching a task fails', async () => {
     jest
       .spyOn(require('react-router-dom'), 'useParams')
       .mockReturnValue({ id: '123' });
+
     (axios.get as jest.Mock).mockRejectedValueOnce(new Error('Network error'));
+
     render(
       <BrowserRouter>
         <Tasks />
       </BrowserRouter>
     );
+
     await waitFor(() => {
       expect(
         screen.getByText(
