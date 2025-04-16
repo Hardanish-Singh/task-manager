7646b383358aeebf94b9afd4f494058ca63d58e7
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-04-16 10:16:47 -0400

    Task Manager

--- apps/frontend/src/app/tests/ViewTasks.spec.tsx
+++ apps/frontend/src/app/tests/ViewTasks.spec.tsx
@@ -12,6 +12,11 @@ import { Status } from '../types/types';
 
 jest.mock('axios');
 
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: jest.fn().mockReturnValue(() => jest.fn()),
+}));
+
 const tasksMock = [
   {
     id: 'cd6c9362-96cb-4604-9506-c36dc6e2974f',
@@ -40,24 +45,26 @@ describe('ViewTasks', () => {
   });
 
   it('should render loading icon while tasks are loading', () => {
-    (axios.get as jest.Mock).mockResolvedValueOnce({
-      data: [],
-    });
+    (axios.get as jest.Mock).mockResolvedValueOnce({ data: [] });
+
     const { getAllByText } = render(
       <BrowserRouter>
         <ViewTasks />
       </BrowserRouter>
     );
+
     expect(getAllByText(/Loading.../i)).toBeTruthy();
   });
 
   it('should display an error message if fetching tasks fails', async () => {
     (axios.get as jest.Mock).mockRejectedValueOnce(new Error('Network error'));
+
     render(
       <BrowserRouter>
         <ViewTasks />
       </BrowserRouter>
     );
+
     await waitFor(() => {
       expect(
         screen.getByText(
@@ -68,21 +75,24 @@ describe('ViewTasks', () => {
   });
 
   it('should render tasks in a table when tasks are fetched successfully', async () => {
-    (axios.get as jest.Mock).mockResolvedValue({
-      data: tasksMock,
-    });
+    (axios.get as jest.Mock).mockResolvedValue({ data: tasksMock });
+
     render(
       <BrowserRouter>
         <ViewTasks />
       </BrowserRouter>
     );
+
     await waitFor(() => {
       expect(screen.getByText('title 1')).toBeTruthy();
       expect(screen.getByText('description 1')).toBeTruthy();
       expect(screen.getByText('title 2')).toBeTruthy();
       expect(screen.getByText('description 2')).toBeTruthy();
       expect(screen.getByText('CLOSED')).toBeTruthy();
+      expect(screen.getByText('Create Task'));
     });
+
+    fireEvent.click(screen.getByText('Create Task'));
   });
 
   it('should sort tasks by title when title column is clicked', async () => {
@@ -91,62 +101,77 @@ describe('ViewTasks', () => {
         <ViewTasks />
       </BrowserRouter>
     );
+
     await waitFor(() => {
       expect(screen.getByText('title 2')).toBeTruthy();
       expect(screen.getByText('title 1')).toBeTruthy();
     });
+
     fireEvent.click(screen.getByText('Title'));
+
     expect(screen.getByText('title 1')).toBeTruthy();
     expect(screen.getByText('title 2')).toBeTruthy();
   });
 
   it('should delete a task from the table', async () => {
-    (axios.get as jest.Mock).mockResolvedValueOnce({
-      data: tasksMock,
-    });
+    (axios.get as jest.Mock).mockResolvedValueOnce({ data: tasksMock });
+
     (axios.delete as jest.Mock).mockResolvedValueOnce({});
+
     render(
       <BrowserRouter>
         <ViewTasks />
       </BrowserRouter>
     );
+
     const title1Element = (await screen.findByText('title 1')) as any;
+
     expect(await screen.findByText('title 2')).toBeTruthy();
+
     const row = title1Element.closest('tr');
+
     if (!row) {
       throw new Error("Could not find table row for 'title 1'");
     }
+
     const deleteIcon = within(row).getByTestId(`delete-icon`);
+
     fireEvent.click(deleteIcon);
+
     const deleteButton = screen.getByTestId('delete-button');
+
     fireEvent.click(deleteButton);
+
     await waitFor(() => {
       expect(screen.queryByText('title 1')).toBeNull();
     });
+
     expect(screen.getByText('title 2')).toBeTruthy();
   });
 
   it('should allow searching tasks by title or description', async () => {
-    (axios.get as jest.Mock).mockResolvedValueOnce({
-      data: tasksMock,
-    });
+    (axios.get as jest.Mock).mockResolvedValueOnce({ data: tasksMock });
+
     render(
       <BrowserRouter>
         <ViewTasks />
       </BrowserRouter>
     );
+
     await waitFor(() => {
       const searchInput = screen.getByPlaceholderText(
         'Search Title, Description...'
       );
+
       const filteredTasks = tasksMock.filter(
         (task) =>
           task.title.includes('title 1') || task.description.includes('title 1')
       );
-      (axios.get as jest.Mock).mockResolvedValueOnce({
-        data: filteredTasks,
-      });
+
+      (axios.get as jest.Mock).mockResolvedValueOnce({ data: filteredTasks });
+
       fireEvent.change(searchInput, { target: { value: 'title 1' } });
+
       expect(screen.getByText('title 1')).toBeTruthy();
       expect(screen.queryByText('title 2')).not.toBeTruthy();
     });
