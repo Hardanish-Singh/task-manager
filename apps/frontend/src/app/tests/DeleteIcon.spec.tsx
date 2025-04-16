994056595098688588f991ea08c440d9e45bdeb3
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-04-16 09:52:43 -0400

    Task Manager

--- apps/frontend/src/app/tests/DeleteIcon.spec.tsx
+++ apps/frontend/src/app/tests/DeleteIcon.spec.tsx
@@ -1,24 +1,18 @@
 import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import DeleteIcon from '../components/DeleteIcon';
 
-const mockUpdateTasks = jest.fn();
-
-jest.spyOn(console, 'warn').mockImplementation(() => {});
-jest.spyOn(console, 'error').mockImplementation(() => {});
-
 describe('DeleteIcon', () => {
   const taskId = 'cd6c9362-96cb-4604-9506-c36dc6e2974f';
   const taskTitle = 'Test Task';
-
-  beforeEach(() => {
-    jest.clearAllMocks();
-  });
+  const mockUpdateTasks = jest.fn();
 
   it('should show the ConfirmDeletion modal when the delete icon is clicked', () => {
     render(
       <DeleteIcon id={taskId} title={taskTitle} updateTasks={mockUpdateTasks} />
     );
+
     fireEvent.click(screen.getByTestId('delete-icon'));
+
     expect(screen.getByText(`Delete ${taskTitle}`)).toBeTruthy();
   });
 
@@ -26,8 +20,10 @@ describe('DeleteIcon', () => {
     render(
       <DeleteIcon id={taskId} title={taskTitle} updateTasks={mockUpdateTasks} />
     );
+
     fireEvent.click(screen.getByTestId('delete-icon'));
     fireEvent.click(screen.getByText('Cancel'));
+
     await waitFor(() => {
       expect(mockUpdateTasks).not.toHaveBeenCalled();
     });
@@ -37,9 +33,12 @@ describe('DeleteIcon', () => {
     render(
       <DeleteIcon id={taskId} title={taskTitle} updateTasks={mockUpdateTasks} />
     );
+
     fireEvent.click(screen.getByTestId('delete-icon'));
     expect(screen.getByText(`Delete ${taskTitle}`)).toBeTruthy();
+
     fireEvent.click(screen.getByText('Cancel'));
+
     expect(screen.queryByText(`Delete ${taskTitle}`)).toBeFalsy();
   });
 });
