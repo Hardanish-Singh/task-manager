581db8d57367894684330f1b39be931114c012dd
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2026-01-11 12:40:07 -0500

    fix: code formatting

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -7,6 +7,7 @@ const host = process.env.HOST ?? '0.0.0.0';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
 const app = express();
+
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
